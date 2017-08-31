46c91cfd1ccb4beceed4ffb8c6c1dc3859c45fe4
Vladimir Novikov <NovikovVV@users.noreply.github.com>
2017-08-31 18:03:49 +0300

    Скрипты

--- js/script.js
+++ js/script.js
@@ -1,4 +1,19 @@
 var servisesLabel = document.getElementsByClassName("servises__label");
+var cartPopup = document.querySelector(".cart-popup");
+var buy = document.querySelectorAll(".buy");
+var cartPopupClose = document.querySelector(".cart-popup .close");
+var feedbackLink = document.querySelector(".contacts .btn");
+var feedbackForm = document.querySelector(".feedback-popup");
+var mapLink = document.querySelector(".map__link");
+var mapPopup = document.querySelector(".map-popup");
+var close = document.querySelector(".close");
+var feedbackClose = document.querySelector(".feedback-popup .close");
+var html = document.documentElement;
+var servisesLabel = document.getElementsByClassName("servises__label");
+var cartClose = document.querySelector(".cart-popup .close");
+
+html.className = html.className.replace("js-off","js-on");
+
 for (var i = 0; i < servisesLabel.length; i++) {
     servisesLabel[i].addEventListener("click", function(){
       if(!(this.classList.contains("active"))) {
@@ -10,15 +25,28 @@ for (var i = 0; i < servisesLabel.length; i++) {
     })
 }
 
-var feedbackLink = document.querySelector(".contacts .btn");
-var feedbackForm = document.querySelector(".feedback-popup");
-var mapLink = document.querySelector(".map__link");
-var mapPopup = document.querySelector(".map-popup");
-var close = document.querySelector(".close");
-var feedbackClose = document.querySelector(".feedback-popup .close");
-var html = document.documentElement;
+[].forEach.call(buy, function(buy){
+    buy.addEventListener("click", function (event) {
+      event.preventDefault();
+      cartPopup.classList.add("modal-content-show");
+    });
+});
 
-html.className = html.className.replace("js-off","js-on");
+close.addEventListener("click", function (event) {
+    event.preventDefault();
+    cartPopup.classList.remove("modal-content-show");
+});
+
+for (var i = 0; i < servisesLabel.length; i++) {
+    servisesLabel[i].addEventListener("click", function(){
+      if(!(this.classList.contains("active"))) {
+        for(var i = 0; i < servisesLabel.length; i++) {
+          servisesLabel[i].classList.remove("active");
+        }
+        this.classList.add("active");
+      }
+    })
+}
 
 feedbackLink.addEventListener("click", function(event) {
 	event.preventDefault();
@@ -32,7 +60,6 @@ mapLink.addEventListener("click", function(event) {
 
 close.addEventListener("click", function(event) {
 	event.preventDefault();
-	feedbackForm.classList.remove("modal-content-show");
   mapPopup.classList.remove("modal-content-show");
 });
 
